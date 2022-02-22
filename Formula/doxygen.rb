3b17836b1662ddc0a1b5848ee64db6e25df550ca
Jason Byun <tmefmd@gmail.com>
2022-02-21 19:07:58 -0800

    Change doxygen file

--- Formula/doxygen.rb
+++ Formula/doxygen.rb
@@ -1,4 +1,4 @@
-class DoxygenAT18 < Formula
+class Doxygen < Formula
     desc "Generate documentation for several programming languages"
     homepage "https://www.doxygen.org/"
     url "https://doxygen.nl/files/doxygen-1.9.2.src.tar.gz"
